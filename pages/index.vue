5457e80321201f5266c19566bfaabcb93db34a1a
David Schubert <github@davidschubert.com>
2022-08-27 00:00:01 -1000

    Hamburg

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
 <template>
-	<div class="grid h-screen place-content-center gap-3"></div>
+	<div class="grid h-screen place-content-center gap-3">
+		<h1 class="text-5xl text-blue-700">
+			Welcome to Nuxt 3 with Content v2
+		</h1>
+		<p class="text-xl text-red-600">Das ist ein kleiner Text.</p>
+		<nuxt-link to="/blog" class="mx-auto rounded-xl bg-amber-500">
+			Go to Blog
+		</nuxt-link>
+	</div>
 </template>
